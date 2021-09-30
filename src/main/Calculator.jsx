c80123385cd4720177e5a143cafe5f79e497a737
juliomarquesjr <juliomarquesjr@yahoo.com.br>
2021-09-30 18:02:14 -0300

    Ajustes nos botões

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,27 +4,42 @@ import Button from "../components/Button";
 import Display from "../components/Display";
 
 function Calculadora() {
+  function clearMemory() {
+    console.log("Limpar");
+  }
+
+  function setOperation(oper) {
+    console.log("Operação: " + oper);
+  }
+
+  function addDigit(digit) {
+    console.log("Digito: " + digit);
+  }
+
   return (
     <React.Fragment>
       <div className="calculator">
-        <Display value={100}/>
-        <Button label="AC" />
-        <Button label="/" />
-        <Button label="7" />
-        <Button label="8" />
-        <Button label="9" />
-        <Button label="*" />
-        <Button label="4" />
-        <Button label="5" />
-        <Button label="6" />
-        <Button label="-" />
-        <Button label="1" />
-        <Button label="2" />
-        <Button label="3" />
-        <Button label="+" />
-        <Button label="0" />
-        <Button label="." />
-        <Button label="=" />
+        <Display value={100} />
+        <Button label="AC" triple click={() => clearMemory()} />
+
+        {/* Apenas uma forma diferente de declarar usando Arrowfunction*/}
+        <Button label="/" operation click={(event) => setOperation(event)} />
+
+        <Button label="7" click={addDigit} />
+        <Button label="8" click={addDigit} />
+        <Button label="9" click={addDigit} />
+        <Button label="*" operation click={setOperation} />
+        <Button label="4" click={addDigit} />
+        <Button label="5" click={addDigit} />
+        <Button label="6" click={addDigit} />
+        <Button label="-" operation click={setOperation} />
+        <Button label="1" click={addDigit} />
+        <Button label="2" click={addDigit} />
+        <Button label="3" click={addDigit} />
+        <Button label="+" operation click={setOperation} />
+        <Button label="0" double click={addDigit} />
+        <Button label="." click={addDigit} />
+        <Button label="=" operation click={setOperation} />
       </div>
     </React.Fragment>
   );
