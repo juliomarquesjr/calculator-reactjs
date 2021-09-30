3e57f6773da615602cb0eff16c06a0f3b1696750
juliomarquesjr <juliomarquesjr@yahoo.com.br>
2021-09-30 16:38:36 -0300

    Component Calculadora e estilo

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Calculator.css";
+
+function Calculadora() {
+  return (
+    <React.Fragment>
+      <div className="calculator"></div>
+    </React.Fragment>
+  );
+}
+export default Calculadora;
